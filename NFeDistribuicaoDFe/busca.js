ab7013b65485d7b1018ffc584b40b41bb3a137cc
Pablo George <pablogeokar@hotmail.com>
2021-07-06 11:48:02 -0300

    salvando xml

---
+++ NFeDistribuicaoDFe/busca.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const soap = require('soap');
+
+const args = {
+  distDFeInt: {
+    attributes: {
+      xmlns: 'http://www.portalfiscal.inf.br/nfe',
+      versao: '1.01',
+    },
+    tpAmb: 1,
+    cUFAutor: 29,
+    CNPJ: '96808993000100',
+    distNSU: {
+      ultNSU: '000000000000000'
+    }
+  }
+
+}
+
+var options = {
+  wsdl_options: {
+    rejectUnauthorized: false,
+    strictSSL: false,
+    pfx: fs.readFileSync('./certs/LUIZ_CARLOS.p12'),
+    //ca: fs.readFileSync('./certs/ca.pem'), // Autoridade certificadora Receita Federal
+    //key: fs.readFileSync('./certs/key.pem'),
+    passphrase: '1234',
+    //securityOptions: 'SSL_OP_NO_SSLv3'
+  }
+}
+
+const url = 'https://www1.nfe.fazenda.gov.br/NFeDistribuicaoDFe/NFeDistribuicaoDFe.asmx?wsdl';
+
+soap.createClient(url, options, (err, client) => {
+  err && console.log('ERRO1', err)
+
+  //client.addHttpHeader('Content-Type', `text/xml; charset=utf-8`);
+
+  client.setSecurity(new soap.ClientSSLSecurityPFX('./certs/LUIZ_CARLOS.p12', '1234', {
+    rejectUnauthorized: false,
+    strictSSL: false,
+    //secureOptions: 'SSL_OP_NO_TLSv1_2',
+    securityOptions: 'SSL_OP_NO_SSLv3',
+    forever: true
+  }));
+
+  client.nfeDistDFeInteresse({ nfeDadosMsg: args }, (err, result) => {
+    err && console.log('ERRO2', err)
+
+    console.log('>>>\n', JSON.stringify(result, null, 4), '\n<<<');
+  })
+
+})
+
---
+++ NFeDistribuicaoDFe/index.js
@@ -0,0 +1,32 @@
+const zlib = require('zlib');
+const fs = require('fs');
+const crypto = require('crypto')
+
+const json = require('./dados.json')
+const dados = json[0].$value
+
+async function salvaXML(value) {
+  const buf = Buffer.from(value, 'base64')
+
+  zlib.unzip(buf, function (err, buffer) {
+    if (err) throw err;
+
+    let content = buffer.toString('utf8');
+    let hash = crypto.createHash('md5').update(content).digest('hex');
+
+    fs.writeFile(`./xmls/${hash}.xml`, content, function (err) {
+      if (err) throw err;
+    });
+  });
+
+}
+
+module.exports = salvaXML
+
+
+
+
+
+
+
+
