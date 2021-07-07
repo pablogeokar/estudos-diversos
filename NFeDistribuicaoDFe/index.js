8a4e92cbf049ac5875e36c948764b9eadec8cdd3
Pablo George <pablogeokar@hotmail.com>
2021-07-07 18:44:25 -0300

    baixando todas as notas

--- NFeDistribuicaoDFe/index.js
+++ NFeDistribuicaoDFe/index.js
@@ -1,32 +1,66 @@
-const zlib = require('zlib');
 const fs = require('fs');
-const crypto = require('crypto')
+const soap = require('soap');
+const salvaXML = require('./salvaXML')
 
-const json = require('./dados.json')
-const dados = json[0].$value
+const url = 'https://www1.nfe.fazenda.gov.br/NFeDistribuicaoDFe/NFeDistribuicaoDFe.asmx?wsdl'
 
-async function salvaXML(value) {
-  const buf = Buffer.from(value, 'base64')
-
-  zlib.unzip(buf, function (err, buffer) {
-    if (err) throw err;
-
-    let content = buffer.toString('utf8');
-    let hash = crypto.createHash('md5').update(content).digest('hex');
-
-    fs.writeFile(`./xmls/${hash}.xml`, content, function (err) {
-      if (err) throw err;
-    });
-  });
-
+const args = {
+  distDFeInt: {
+    attributes: {
+      xmlns: 'http://www.portalfiscal.inf.br/nfe',
+      versao: '1.01',
+    },
+    tpAmb: 1,
+    cUFAutor: 29,
+    CNPJ: '01957614000129',
+    distNSU: {
+      ultNSU: '000000000000000'
+    }
+  }
 }
 
-module.exports = salvaXML
+const options = {
+  wsdl_options: {
+    rejectUnauthorized: false,
+    strictSSL: false,
+    pfx: fs.readFileSync('./certs/certificadoDB.pfx'),
+    passphrase: '1234',
+  }
+}
 
+soap.createClient(url, options, (err, client) => {
+  err && console.log('ERRO1', err)
 
+  client.setSecurity(new soap.ClientSSLSecurityPFX('./certs/certificadoDB.pfx', '1234', {
+    rejectUnauthorized: false,
+    strictSSL: false,
+    securityOptions: 'SSL_OP_NO_SSLv3',
+    forever: true
+  }));
 
+  client.nfeDistDFeInteresse({ nfeDadosMsg: args }, (err, result) => {
+    err && console.log('ERRO2', err)
 
+    //let dados = JSON.stringify(result, null, 4)
+    let dados = result.nfeDistDFeInteresseResult.retDistDFeInt.loteDistDFeInt.docZip
+    let ultNSU = result.nfeDistDFeInteresseResult.retDistDFeInt.ultNSU
+    let maxNSU = result.nfeDistDFeInteresseResult.retDistDFeInt.maxNSU
 
+    if (Array.isArray(dados)) {
+      dados.map(item => {
+        let schema = item.attributes.schema
+        if (schema.substr(0, 7) === 'procNFe') {
+          salvaXML(item.$value)
+        }
+      })
+    }
 
+    // Salva o arquivo de retorno da consulta
+    fs.writeFile(`./result.json`, JSON.stringify(result, null, 4), function (err) {
+      if (err) throw err;
+    });
 
-
+    //console.log('>>>\n', JSON.stringify(result, null, 4), '\n<<<');
+    console.log('Consulta finalizada')
+  })
+})
